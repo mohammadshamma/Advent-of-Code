ba82434a4e934edd9280762886037cb9d01e313b
Mohammad Shamma <mohammadshamma@gmail.com>
2021-12-04 05:04:12 -0800

    Day 4 part 2!!!!

--- days/4/solution.py
+++ days/4/solution.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3 
 
+
 class Board(object):
   def __init__(self, value_rows):
       self.value_rows = value_rows
       self.mark_rows = [[False for col in range(5)] for row in range(5)]
-
+      self.solved = False
 
   def MarkAndCheck(self, value):
     for row in range(5):
@@ -12,7 +13,11 @@ class Board(object):
         if self.value_rows[row][col] == value:
           self.mark_rows[row][col] = True
           if all(self.mark_rows[row]) or all([self.mark_rows[i][col] for i in range(5)]):
+            self.solved = True
             return value * sum([sum([self.value_rows[i][j] for j in range(5) if not self.mark_rows[i][j]]) for i in range(5)])
+
+  def IsSolved(self):
+    return self.solved
 
 
 def ParseNumbers(file, sep=None):
@@ -25,23 +30,27 @@ def ParseBoard(file):
   return Board(rows)
 
 
-def SolvePartOne():
+def Main():
+  print('Hello Day 4!')
   file = open('input.txt')
   drawn_numbers = ParseNumbers(file, ',')
   boards = []
+  boards_solved = 0
   while file.readline():
     boards.append(ParseBoard(file))
+  last_solved_board_score = None
   for drawn_number in drawn_numbers:
     for board in boards:
+      if board.IsSolved():
+        continue
       score = board.MarkAndCheck(drawn_number)
-      if score:
-        print(f'winning board score = {score}')
-        return
-
-
-def Main():
-  print('Hello Day 4!')
-  SolvePartOne()
+      if score: 
+        boards_solved += 1
+        if boards_solved == 1:
+          print(f'Winning board score = {score}')
+        last_solved_board_score = score
+  if last_solved_board_score:
+    print(f'Last solved board score = {last_solved_board_score}')
 
 
 if __name__ == '__main__':
