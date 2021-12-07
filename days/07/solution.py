7a0aa4d870ac4257a8b01c26f562f0823e568009
Mohammad Shamma <mohammadshamma@gmail.com>
2021-12-07 02:10:01 -0800

    Get Day 7 to pass big inputs!

--- days/07/solution.py
+++ days/07/solution.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3 
 
 
+import math
+
+
 def GetCrabPositions(path):
   return [int(position) for position in open(path).readline().split(',')]
 
@@ -14,7 +17,7 @@ def CostFunctionV1(crab_position, to_position):
 # 3 -> 6 = 9 - 3 = 6
 def CostFunctionV2(crab_position, to_position):
   distance = abs(crab_position - to_position)
-  return distance**2 - distance * (distance - 1) / 2
+  return int(distance**2 - distance * (distance - 1) / 2)
 
 
 def CalculatePositionCost(crab_positions, position, cost_function):
@@ -24,14 +27,24 @@ def CalculatePositionCost(crab_positions, position, cost_function):
 def CalculateOptimalPosition(crab_positions, cost_function):
   minimum_position = min(crab_positions)
   maximum_position = max(crab_positions)
-  optimal_cost = None
-  optimal_position = None
-  for position in range(minimum_position, maximum_position + 1):
-    cost = CalculatePositionCost(crab_positions, position, cost_function)
-    if not optimal_cost or cost < optimal_cost:
-      optimal_cost = cost
-      optimal_position = position
-  return optimal_position, optimal_cost
+  minimum_position_cost = CalculatePositionCost(crab_positions, minimum_position, cost_function)
+  maximum_position_cost = CalculatePositionCost(crab_positions, maximum_position, cost_function)
+  while minimum_position < maximum_position - 1:
+    middle_position_1 = math.floor((maximum_position + minimum_position)/2)
+    middle_position_2 = middle_position_1 + 1
+    middle_position_1_cost = CalculatePositionCost(crab_positions, middle_position_1, cost_function)
+    middle_position_2_cost = CalculatePositionCost(crab_positions, middle_position_2, cost_function)
+    if middle_position_1_cost < middle_position_2_cost:
+      maximum_position = middle_position_1
+      maximum_position_cost = middle_position_1_cost
+    else:
+      minimum_position = middle_position_2
+      minimum_position_cost = middle_position_2_cost
+
+  if maximum_position_cost < minimum_position_cost:
+    return maximum_position, maximum_position_cost
+  else:
+    return minimum_position, minimum_position_cost
 
 
 def Test():
@@ -60,11 +73,25 @@ def SolvePartTwo():
   assert(optimal_position == 478)
 
 
+def BigBoy():
+  crab_positions = GetCrabPositions('7-1000000-2.in')
+  optimal_position, optimal_cost = CalculateOptimalPosition(crab_positions, CostFunctionV1)
+  print(f'Big Boy Part I: Fuel cost = {optimal_cost}, position = {optimal_position}')
+  assert(optimal_cost == 348121442862)
+  optimal_position, optimal_cost = CalculateOptimalPosition(crab_positions, CostFunctionV2)
+  print(f'Big Boy Part II: Fuel cost = {optimal_cost}, position = {optimal_position}')
+  assert(optimal_cost == 97051441111920642)
+
+
 def Main():
   print('Hello Day 7!')
   Test()
   SolvePartOne()
   SolvePartTwo()
+  # To run the big boy dataset, download the input file from
+  # https://the-tk.com/files/aoc2021-bigboys/7-1000000-2.in.xz
+  # unzip it and uncomment the call below.
+  # BigBoy()
 
 
 if __name__ == '__main__':
