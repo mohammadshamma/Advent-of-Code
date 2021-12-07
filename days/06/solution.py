3b468d9c432697ac600d43a553a4c5285adf4f81
Mohammad Shamma <mohammadshamma@gmail.com>
2021-12-06 21:26:59 -0800

    Ring buffer solution for day 6!!

--- days/06/solution.py
+++ days/06/solution.py
@@ -3,6 +3,28 @@
 from collections import defaultdict
 
 
+class RingIntegerList():
+  def __init__(self, size):
+    self.list = [0] * size
+    self.offset = 0
+
+  def ShiftLeft(self):
+    self.offset += 1
+    if(self.offset >= len(self.list)):
+      self.offset = 0
+
+  def _GetRealIndex(self, index, offset):
+    return (index + offset) % len(self.list)
+
+  def GetValue(self, index):
+    real_index = self._GetRealIndex(index, self.offset)
+    return self.list[real_index]
+
+  def BumpValue(self, index, value):
+    real_index = self._GetRealIndex(index, self.offset)
+    self.list[real_index] += value
+
+
 def ReadFishAgeList(path):
   fish_age_list_str = None
   with open(path) as f:
@@ -10,34 +32,33 @@ def ReadFishAgeList(path):
   return [int(fish_age_str) for fish_age_str in fish_age_list_str]
 
 
-def CalculateFishCountAfterNDays(day_count):
-  fish_age_list = ReadFishAgeList('input.txt')
-  fish_age_buckets = [0] * 9
+def CalculateFishCountAfterNDays(day_count, fish_age_list):
+  fish_age_buckets = RingIntegerList(9)
   for fish_age in fish_age_list:
-    fish_age_buckets[fish_age] += 1
+    fish_age_buckets.BumpValue(fish_age, 1)
   for day in range(day_count):
-    new_fish_age_buckets = [0] * 9
-    # Process zero days remaining in cycle.
-    new_fish_age_buckets[8] += fish_age_buckets[0]
-    new_fish_age_buckets[6] += fish_age_buckets[0]
-    # Process non-zero days.
-    for day in range(1, 9):
-      new_fish_age_buckets[day - 1] += fish_age_buckets[day]
-    fish_age_buckets = new_fish_age_buckets
-  return sum(fish_age_buckets)
+    zero_fishes = fish_age_buckets.GetValue(0)
+    fish_age_buckets.ShiftLeft()
+    fish_age_buckets.BumpValue(6, zero_fishes)
+  return sum(fish_age_buckets.list)
 
 
 def SolvePartOne():
   fish_age_list = ReadFishAgeList('input.txt')
-  fish_count = CalculateFishCountAfterNDays(80)
+  fish_count = CalculateFishCountAfterNDays(80, fish_age_list)
   assert(fish_count == 379414)
   print(f'Number of fish after 80 days: {fish_count}')
 
 
 def SolvePartTwo():
   fish_age_list = ReadFishAgeList('input.txt')
-  fish_count = CalculateFishCountAfterNDays(256)
+  fish_count = CalculateFishCountAfterNDays(256, fish_age_list)
   assert(fish_count == 1705008653296)
+  print(f'Number of fish after 256 days: {fish_count}')
+
+
+def BigBoy():
+  fish_count = CalculateFishCountAfterNDays(9999999, [3,4,3,1,2])
   print(f'Number of fish after 256 days: {fish_count}')
 
 
@@ -45,6 +66,7 @@ def Main():
   print('Hello Day 6!')
   SolvePartOne()
   SolvePartTwo()
+  BigBoy()
 
 
 if __name__ == '__main__':
