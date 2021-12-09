0246bed12fa739f0db22e5140360210445d162bc
Mohammad Shamma <mohammadshamma@gmail.com>
2021-12-08 22:57:04 -0800

    Day 9 part 2.

--- days/09/solution.py
+++ days/09/solution.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 
+
 def ReadHeightMap(path):
   return np.genfromtxt(path, dtype=int, delimiter=1)
 
@@ -29,9 +30,40 @@ def CalculateLowPointsRiskLevels(path):
   return sum_of_risk_levels
 
 
+def CalculateProductOfTopThreeBasinsSizes(path):
+  a = ReadHeightMap(path)
+  visited = np.zeros(a.shape, dtype=bool)
+  row_count, column_count = a.shape
+  basin_sizes = []
+  for i in range(row_count):
+    for j in range(column_count):
+      if IsLowPoint(a, i, j):
+        # BFS the basin
+        basin_size = 0
+        to_visit = [(i, j)]
+        while to_visit:
+          current_i, current_j = to_visit.pop(0)
+          if visited[current_i, current_j]:
+            continue
+          basin_size += 1
+          visited[current_i, current_j] = True
+          if current_i > 0 and a[current_i - 1, current_j] != 9 and not visited[current_i - 1, current_j]:
+            to_visit.append((current_i - 1, current_j))
+          if current_i < row_count - 1 and a[current_i + 1, current_j] != 9 and not visited[current_i + 1, current_j]:
+            to_visit.append((current_i + 1, current_j))
+          if current_j > 0 and a[current_i, current_j - 1] != 9 and not visited[current_i, current_j - 1]:
+            to_visit.append((current_i, current_j - 1))
+          if current_j < column_count - 1 and a[current_i, current_j + 1] != 9 and not visited[current_i, current_j + 1]:
+            to_visit.append((current_i, current_j + 1))
+        basin_sizes.append(basin_size)
+  return np.prod(sorted(basin_sizes, reverse=True)[:3])
+
+
 def Test():
   sum_of_risk_levels = CalculateLowPointsRiskLevels('test.txt')
   assert(sum_of_risk_levels == 15)
+  product_of_top_three_basins_size = CalculateProductOfTopThreeBasinsSizes('test.txt')
+  assert(product_of_top_three_basins_size == 1134)
 
 
 def SolvePartOne():
@@ -39,11 +71,16 @@ def SolvePartOne():
   print(f'Sum of risk levels = {sum_of_risk_levels}')
 
 
+def SolvePartTwo():
+  p = CalculateProductOfTopThreeBasinsSizes('height_map.txt')
+  print(f'Product of size of top three basins: {p}')
+
+
 def Main():
   print('Hello Day 9!')
   Test()
   SolvePartOne()
-  # SolvePartTwo()
+  SolvePartTwo()
 
 
 if __name__ == '__main__':
