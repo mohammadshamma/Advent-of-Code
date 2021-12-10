83f073c14cf069db38836781ef2e73f1011c9235
Mohammad Shamma <mohammadshamma@gmail.com>
2021-12-10 01:23:05 -0800

    Day 10 Part 2

--- days/10/solution.py
+++ days/10/solution.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3 
 
+import statistics
+
+
 OPENERS = '([<{'
 
 
@@ -8,6 +11,14 @@ WRONG_CLOSER_TO_PENALTY = {
   ']': 57,
   '}': 1197,
   '>': 25137
+}
+
+
+COMPLETION_SCORE = {
+  ')': 1,
+  ']': 2,
+  '}': 3,
+  '>': 4
 }
 
 
@@ -21,13 +32,17 @@ OPENER_TO_CLOSER = {
 
 CLOSER_TO_OPENER = {v: k for k, v in OPENER_TO_CLOSER.items()}
 
+COMPLETE = 'complete'
+INCOMPLETE = 'incomplete'
+CORRUPT = 'corrupt'
+
 
 def GetLines(path):
   with open(path) as f:
     return f.readlines()
 
 
-def CalculateCorruptionScore(line):
+def GetLineState(line):
   stack = []
   for c in line:
     if c in OPENERS:
@@ -35,8 +50,28 @@ def CalculateCorruptionScore(line):
     elif len(stack) and CLOSER_TO_OPENER[c] == stack[-1]:
       stack.pop()
     else:
-      return WRONG_CLOSER_TO_PENALTY[c]
+      return CORRUPT, c
+  if len(stack):
+    return INCOMPLETE, stack
+  return COMPLETE, None
+
+
+def CalculateCorruptionScore(line):
+  state, extra = GetLineState(line)
+  if state == CORRUPT:
+    return WRONG_CLOSER_TO_PENALTY[extra]
   return 0
+  
+
+def CalculateCompletionScore(line):
+  state, extra = GetLineState(line)
+  if state == INCOMPLETE:
+    score = 0
+    for c in reversed(extra):
+      score *= 5
+      score += COMPLETION_SCORE[OPENER_TO_CLOSER[c]]
+    return score
+  return None
 
 
 def CalculateTotalCorruptionScore(path):
@@ -44,9 +79,18 @@ def CalculateTotalCorruptionScore(path):
   return sum([CalculateCorruptionScore(line.strip()) for line in lines])
 
 
+def CalculateTotalCompletionScore(path):
+  lines = GetLines(path)
+  scores = [CalculateCompletionScore(line.strip()) for line in lines]
+  scores = filter(lambda score: score is not None, scores)
+  return statistics.median(scores)
+
+
 def Test():
   error_score = CalculateTotalCorruptionScore('example.txt')
   assert(error_score == 26397)
+  completion_score = CalculateTotalCompletionScore('example.txt')
+  assert(completion_score == 288957)
 
 
 def SolvePartOne():
@@ -55,10 +99,17 @@ def SolvePartOne():
   assert(error_score == 266301)
 
 
+def SolvePartTwo():
+  completion_score = CalculateTotalCompletionScore('input.txt')
+  print(f'completion score = {completion_score}')
+  assert(completion_score == 3404870164)
+
+
 def Main():
   print('Hello Day 10!')
   Test()
   SolvePartOne()
+  SolvePartTwo()
 
 
 if __name__ == '__main__':
