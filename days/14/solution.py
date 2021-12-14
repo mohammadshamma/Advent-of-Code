3fb21f93f235d23f93e92198ac04d335a1aa0b64
Mohammad Shamma <mohammadshamma@gmail.com>
2021-12-14 01:19:58 -0800

    Day 14 Part 2!!

--- days/14/solution.py
+++ days/14/solution.py
@@ -22,49 +22,70 @@ def ReadInput(path):
     return initial_sequence, {rule[0]: rule[1] for rule in insertion_rules}
 
 
-def CalculateNextSequence(sequence, insertion_rules):
-  new_sequence = [sequence[0]]
-  for i in range(len(sequence) - 1):
-    insertion = insertion_rules.get(sequence[i:i+2], '')
-    new_sequence.append(insertion)
-    new_sequence.append(sequence[i+1])
-  return ''.join(new_sequence)
+class PolymerCalculator(object):
+
+  def __init__(self, insertion_rules):
+    self._insertion_rules = insertion_rules
+    self._character_count_memoization = {}
+
+  def AddCountersToAccumulator(accumulator, counters):
+    for k, v in counters.items():
+      accumulator[k] += v
+
+  def CalculateCharacterCountForCharacterTupleAtLevelN(self, tuple, steps_ahead):
+    character_count = self._character_count_memoization.get((tuple, steps_ahead), None)
+    if character_count:
+      return character_count
+    character_count = defaultdict(lambda: 0)
+    if steps_ahead == 0:
+      character_count[tuple[0]] += 1
+    else:
+      inserted_char = self._insertion_rules[tuple]
+      left_tuple = tuple[0] + inserted_char
+      right_tuple = inserted_char + tuple[1]
+      left_character_count = self.CalculateCharacterCountForCharacterTupleAtLevelN(left_tuple, steps_ahead - 1)
+      PolymerCalculator.AddCountersToAccumulator(character_count, left_character_count)
+      right_character_count = self.CalculateCharacterCountForCharacterTupleAtLevelN(right_tuple, steps_ahead - 1)
+      PolymerCalculator.AddCountersToAccumulator(character_count, right_character_count)
+    self._character_count_memoization[(tuple, steps_ahead)] = character_count
+    return character_count
+
+  def CalculateCharacterCountersForSequence(self, sequence, steps):
+    character_count = defaultdict(lambda: 0)
+    for i in range(len(sequence) - 1):
+      current_character_count = self.CalculateCharacterCountForCharacterTupleAtLevelN(sequence[i: i + 2], steps)
+      PolymerCalculator.AddCountersToAccumulator(character_count, current_character_count)
+    character_count[sequence[-1]] += 1
+    return character_count
 
 
-def CalculateNextNSteps(sequence, insertion_rules, n):
-  for step in range(n):
-    sequence = CalculateNextSequence(sequence, insertion_rules)
-  return sequence
-
-
-def CalculateDifferenceBetweenMostAndLeastRecurringCharacters(sequence):
-  character_counters = defaultdict(lambda: 0)
-  for c in sequence:
-    character_counters[c] += 1
-    character_counters
-  max_recurrence = max(character_counters.values())
-  min_recurrence = min(character_counters.values())
+def CalculateDifferenceAfterNStepsV2(path, step_count):
+  sequence, insertion_rules = ReadInput(path)
+  calculator = PolymerCalculator(insertion_rules)
+  character_count = calculator.CalculateCharacterCountersForSequence(sequence, step_count)
+  max_recurrence = max(character_count.values())
+  min_recurrence = min(character_count.values())
   return max_recurrence - min_recurrence
 
 
-def CalculateDifferenceAfter10Steps(path):
-  initial_sequence, insertion_rules = ReadInput(path)
-  sequence = CalculateNextNSteps(initial_sequence, insertion_rules, 10)
-  return CalculateDifferenceBetweenMostAndLeastRecurringCharacters(sequence)
-
-
 def Test():
-  difference = CalculateDifferenceAfter10Steps('example.txt')
+  difference = CalculateDifferenceAfterNStepsV2('example.txt', 10)
+  print(difference)
   assert(difference == 1588)
+  difference = CalculateDifferenceAfterNStepsV2('example.txt', 40)
+  assert(difference == 2188189693529)
 
 
 def SolvePartOne():
-  difference = CalculateDifferenceAfter10Steps('input.txt')
-  print(f'Difference between most and least recurring character = {difference}')
+  difference = CalculateDifferenceAfterNStepsV2('input.txt', 10)
+  print(f'Difference between most and least recurring character after 10 steps = {difference}')
+  assert(difference == 2988)
 
 
 def SolvePartTwo():
-  pass
+  difference = CalculateDifferenceAfterNStepsV2('input.txt', 40)
+  print(f'Difference between most and least recurring character after 40 steps = {difference}')
+  assert(difference == 3572761917024)
 
 
 def Main():
