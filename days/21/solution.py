a806903420683b217ac8ff89b36b8e2367ee51d7
Mohammad Shamma <mohammadshamma@gmail.com>
2021-12-21 04:11:52 -0800

    Day 21, part 2!!! ☃️☃️☃️

--- days/21/solution.py
+++ days/21/solution.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3 
 
+
+from collections import defaultdict
+
 class PredictableDice(object):
 
   def __init__(self):
@@ -60,10 +63,49 @@ class Game(object):
     return self.players[loser_index].GetScore() * self.dice.GetRollCount()
 
 
+def CalculateWinningScenarios(players_pos, players_score, next_player, depth=0, cache={}):
+
+  invocation_tuple = (players_pos[0], players_pos[1], players_score[0], players_score[1], next_player)
+  if invocation_tuple in cache:
+    return cache[invocation_tuple]
+
+  scenario_counts = defaultdict(lambda: 0)
+  for roll1 in range(1,4):
+    for roll2 in range(1,4):
+      for roll3 in range(1,4):
+        scenario_counts[sum([roll1, roll2, roll3])] += 1
+  
+  player1_winning_outcomes = 0
+  player2_winning_outcomes = 0
+  for roll_sum, count in scenario_counts.items():
+    # print(f'{"".join(["  "] * depth)}player {next_player} rolls {roll_sum} from {players_pos[next_player]}')
+    new_pos = ((players_pos[next_player] + roll_sum  - 1) % 10) + 1
+    new_score = players_score[next_player] + new_pos
+    # print(f'{"".join(["  "] * depth)}player {next_player} new score {new_score} at {new_pos}')
+    # print(f'{"".join(["  "] * depth)}players_score = {players_score}')
+    if new_score >= 21:
+      if next_player == 0:
+        player1_winning_outcomes += count
+      else:
+        player2_winning_outcomes += count
+    else:
+      next_players_pos = players_pos.copy()
+      next_players_score = players_score.copy()
+      next_players_pos[next_player] = new_pos
+      next_players_score[next_player] = new_score
+      player1_wins, player2_wins = CalculateWinningScenarios(next_players_pos, next_players_score, (next_player + 1) % 2, depth=depth+1, cache=cache)
+      player1_winning_outcomes += (player1_wins * count)
+      player2_winning_outcomes += (player2_wins * count)
+
+  cache[invocation_tuple] = (player1_winning_outcomes, player2_winning_outcomes)
+  return player1_winning_outcomes, player2_winning_outcomes
+
+
 def Test():
   g = Game(4, 8)
   g.Run()
   assert(g.GetLoserScoreAndRollCountProduct() == 739785)
+  assert(CalculateWinningScenarios([4,8], [0,0], 0) == (444356092776315, 341960390180808))
 
 
 def SolvePartOne():
@@ -73,8 +115,9 @@ def SolvePartOne():
 
 
 def SolvePartTwo():
-  pass
-
+  scenario_counts = CalculateWinningScenarios([2,7], [0,0], 0)
+  print(f'Part 2: Winners scenario count = {max(scenario_counts)}')
+  assert(max(scenario_counts) == 133029050096658)
 
 def Main():
   print('Hello Day 21!')
